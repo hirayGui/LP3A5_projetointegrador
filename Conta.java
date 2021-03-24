d045e9ad0c88516aaf2d64e29d930ebb65209c15
Guilherme Brito <hiraygui@gmail.com>
2021-03-24 18:58:37 -0300

    adicionando método na classe Conta para atualizar o saldo após a declaração de uma transação

--- Conta.java
+++ Conta.java
@@ -3,35 +3,38 @@ package projeto_integrador;
 public class Conta {
 
 	private String nome, numero_conta;
-	private double saldo;
+	private double saldo, salario;
 	
-	public Conta(String nome, String numero_conta, double saldo) {
+	public Conta(String nome, String numero_conta, double saldo, double salario) {
 		this.nome = nome;
 		this.numero_conta = numero_conta;
 		this.saldo = saldo;
+		this.salario = salario;
+	}
+	
+	public void updateSaldo(String tipo, double valor) {
+		if(tipo == "entrada" || tipo == "Entrada") {
+			this.saldo = this.saldo + valor;
+		}else
+			if(tipo == "saida" || tipo == "Saida") {
+				this.saldo = this.saldo - valor;
+			}
 	}
 	
 	public String getNome() {
 		return nome;
 	}
 	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
 	public String getNumero_conta() {
 		return numero_conta;
 	}
 	
-	public void setNumero_conta(String numero_conta) {
-		this.numero_conta = numero_conta;
-	}
-	
 	public double getSaldo() {
 		return saldo;
+	}	
+	
+	public double getSalario() {
+		return salario;
 	}
 	
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
 }
